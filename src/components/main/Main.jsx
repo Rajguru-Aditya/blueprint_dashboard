4ffce27cc420c7f864c58407f5951f5500648c92
Aditya Rajguru <77228474+Rajguru-Aditya@users.noreply.github.com>
2023-06-07 12:15:11 +0530

    Create responsive sidebar

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import Sidebar from '../sidebar/Sidebar';
-import './Main.css';
+import React, { useEffect, useState } from "react";
+import Sidebar from "../sidebar/Sidebar";
+import "./Main.css";
+import { Icon } from "@blueprintjs/core";
 
 const MainContent = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [toggleSidebar, setToggleSidebar] = useState(true);
+
+  const toggle = () => {
+    setToggleSidebar(!toggleSidebar);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+  }, []);
+
+  useEffect(() => {
+    if (screenWidth < 768) {
+      setToggleSidebar(false);
+    } else {
+      setToggleSidebar(true);
+    }
+  }, [screenWidth]);
+
   return (
     <div className="main-content-wrapper">
-      <Sidebar />
+      {/* Sidebar hamburger */}
+      <div>
+        <button className="hamburger" onClick={toggle}>
+          <Icon icon="menu" iconSize={20} />
+        </button>
+      </div>
+      <Sidebar toggle={toggleSidebar} />
       <div className="main-content">
         <div className="tables-container">
           <div className="table">
             <table>
               <thead>
                 <tr>
-                  <th colSpan="2" rowSpan="2">Sales today</th>
+                  <th colSpan="2" rowSpan="2">
+                    Sales today
+                  </th>
                 </tr>
               </thead>
               <tbody>
-                <tr className = 'bg'>
+                <tr className="bg">
                   <td>Invoices</td>
                   <td>Total</td>
                 </tr>
-                <tr className='foot'>
+                <tr className="foot">
                   <td>0</td>
                   <td>₹0.00</td>
                 </tr>
@@ -31,15 +63,17 @@ const MainContent = () => {
             <table>
               <thead>
                 <tr>
-                  <th colSpan="2" rowSpan="2">Payments</th>
+                  <th colSpan="2" rowSpan="2">
+                    Payments
+                  </th>
                 </tr>
               </thead>
               <tbody>
-                <tr className = 'bg'>
+                <tr className="bg">
                   <td>Receivables</td>
                   <td>Payables</td>
                 </tr>
-                <tr className='foot'>
+                <tr className="foot">
                   <td>₹0.00</td>
                   <td>₹0.00</td>
                 </tr>
@@ -50,15 +84,17 @@ const MainContent = () => {
             <table>
               <thead>
                 <tr>
-                  <th colSpan="2" rowSpan="2">Inventory</th>
+                  <th colSpan="2" rowSpan="2">
+                    Inventory
+                  </th>
                 </tr>
               </thead>
               <tbody>
-                <tr className = 'bg'>
+                <tr className="bg">
                   <td>Low Stock</td>
                   <td>Out of stock</td>
                 </tr>
-                <tr className='foot'>
+                <tr className="foot">
                   <td>0</td>
                   <td>0</td>
                 </tr>
@@ -69,15 +105,17 @@ const MainContent = () => {
             <table>
               <thead>
                 <tr>
-                  <th colSpan="2" rowSpan="2">Expenses</th>
+                  <th colSpan="2" rowSpan="2">
+                    Expenses
+                  </th>
                 </tr>
               </thead>
               <tbody>
-                <tr className = 'bg'>
+                <tr className="bg">
                   <td>Today</td>
                   <td>Month</td>
                 </tr>
-                <tr className='foot'>
+                <tr className="foot">
                   <td>₹0.00</td>
                   <td>₹0.00</td>
                 </tr>
